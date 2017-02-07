22724b13812a53998f80f529c0894d0d84607094
Nicole Sanders <nsanders9022@gmail.com>
2017-02-06 17:10:23 -0800

    add rest of the code to display the inputted sides and the type of triangle that they make

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -1,5 +1,5 @@
 using Nancy;
-using Parcels.Objects;
+using Triangles.Objects;
 
 namespace Triangles
 {
@@ -7,7 +7,17 @@ namespace Triangles
   {
     public HomeModule()
     {
-      
+      Get["/form"] = _ => {
+        return View["form.cshtml"];
+      };
+      Get["/triangles"] = _ => {
+        TriangleVariables myTriangleVariables = new TriangleVariables();
+        myTriangleVariables.SetSide1(Request.Query["side1"]);
+        myTriangleVariables.SetSide2(Request.Query["side2"]);
+        myTriangleVariables.SetSide3(Request.Query["side3"]);
+        myTriangleVariables.Result();
+        return View["triangles.cshtml", myTriangleVariables];
+      };
     }
   }
 }
--- Objects/TrianglesVariables.cs
+++ Objects/TrianglesVariables.cs
@@ -1,6 +1,65 @@
 namespace Triangles.Objects
 {
-  Public Class Triangles{
-    
+  public class TriangleVariables
+  {
+      private int _side1;
+      private int _side2;
+      private int _side3;
+
+      public int GetSide1()
+      {
+        return _side1;
+      }
+
+      public void SetSide1(int newSide1)
+      {
+        _side1 = newSide1;
+      }
+
+      public int GetSide2()
+      {
+        return _side2;
+      }
+
+      public void SetSide2(int newSide2)
+      {
+        _side2 = newSide2;
+      }
+
+      public int GetSide3()
+      {
+        return _side3;
+      }
+
+      public void SetSide3(int newSide3)
+      {
+        _side3 = newSide3;
+      }
+
+      public string Result()
+      {
+        if((_side1 + _side2 <= _side3) || (_side1 + _side3 <= _side2) || (_side3 + _side2 <= _side1))
+        {
+          return "Not a triangle";
+        }else
+        {
+          if (_side1 == _side2 && _side1 == _side3)
+          {
+            return "Equilateral";
+          }
+          else if ((_side1 == _side2 && _side1 != _side3) || (_side1 == _side3 && _side1 != _side2) || (_side2 == _side3 && _side1 != _side2))
+          {
+            return "Isosceles.";
+          }
+          else if (_side1 != _side2 && _side2 != _side3 && _side1 != _side3)
+          {
+            return "Scalene.";
+          }
+          else
+          {
+            return "not sure what this is";
+          }
+        }
+      }
   }
 }
